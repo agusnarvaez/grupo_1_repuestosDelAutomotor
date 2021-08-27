b3c43abb32379454e3ea51f7eef81a7d78d25478
juanpcarta <juanpcarta@gmail.com>
2021-08-27 18:14:50 -0300

    Agrego archivo register (html + css) y login (html vacío)

--- app.js
+++ app.js
@@ -13,3 +13,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
+app.get('/register', (req, res) => {
+    //let htmlPath = path.resolve(__dirname, './views/register.html');
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    //let htmlPath = path.resolve(__dirname, './views/login.html');
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
