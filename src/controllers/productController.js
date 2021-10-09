853271547e17f2f9fbcc0ee4e94ab23f762b7833
NicoRenz0x <nicolasdirenzo@hotmail.com.ar>
2021-10-09 19:25:42 -0300

    Avance en la subida y carga de archivos de los formularios de creación y edición de productos

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -10,6 +10,7 @@ let partialHead = JSON.parse(fs.readFileSync("src/controllers/partialHead.json",
 /* Array con los productos del sitio */
 const products = JSON.parse(fs.readFileSync('src/data/products.json', 'utf-8'));
 
+
 /* *****Controlador de productos***** */
 const productController = {
     register: function (req, res) { //Página de registro de producto
@@ -18,21 +19,23 @@ const productController = {
     create: function (req, res) { //Creación de producto
         let newProduct = {
             id: products.length + 1,
-            productName: req.body.productName,
+            productName: req.body.name,
             description: req.body.description,
             category: req.body.category,
             price: req.body.price,
-            img: req.body.img,
+            img: '1_' + req.body.name + '.jpg',
         };
+
         products.push(newProduct)
         fs.writeFileSync('src/data/products.json', (JSON.stringify(products)))
-        res.redirect('../');
+        res.redirect('./detail/' + newProduct.id);
     },
     cart: function(req,res) { //Página de carrito de productos
         res.render('./products/cart', { partialHead: partialHead.productCart });
     },
     detail: function(req,res) { //Página de detalle de producto
-        res.render('./products/detail', { partialHead: partialHead.productDetail });
+        let product = products.find(product => product.id == req.params.id);
+        res.render('./products/detail', { partialHead: partialHead.productDetail, product: product });
     },
     edition: function (req, res) { //Página de edición de producto
         let product = products.find(product => product.id == req.params.id);
@@ -44,9 +47,9 @@ const productController = {
         products[product.id - 1].description = req.body.description,
         products[product.id - 1].category = req.body.category,
         products[product.id - 1].price = req.body.price,
-        products[product.id - 1].img = req.body.img,
+        products[product.id - 1].img = '1_' + req.body.name + '.jpg',
         fs.writeFileSync('src/data/products.json', (JSON.stringify(products)))
-        res.redirect('../../');
+        res.redirect('./detail/' + product.id);
     }
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,13 @@
 let express = require('express'); // Solicitud de Express
 let router = express.Router(); //Módulo Router de Express
+const multer = require('multer');
 
 /* *****Controlador de productos***** */
 let productController = require('../controllers/productController.js');
+
+
+
+let imagesCounter = '1' /* Variable que en un futuro usaremos para subir más de una imágen*/
 
 
 /* *****A página carrito**** */
@@ -10,14 +15,34 @@ router.get('/cart', productController.cart);
 
 
 /* *****A página detalle***** */
-router.get('/detail', productController.detail);
+router.get('/detail/:id', productController.detail);
 
 /* *****Métodos para registro de producto ***** */
 router.get("/creation", productController.register); /* A página registro de producto */
-router.put("/creation", productController.create); /* *****Creación de producto***** */
+router.put("/creation",
+ multer(
+    multer.diskStorage({   
+        destination: function (req, file, cb){
+        cb(null, '/data/productsImages')
+    },
+    filename: function (req, file, cb){
+        cb(null, (imagesCounter + '_' + req.body.productName + '.jpg'))
+    }
+})).single('image'),
+ productController.create); /* *****Creación de producto***** */
 
 /* *****Métodos para registro de producto ***** */
 router.get("/edition/:id", productController.edition); /* A página edición de producto */
-router.put("/edition", productController.edit); /* *****Edición de producto***** */
+router.put("/edition", 
+multer(
+    multer.diskStorage({   
+        destination: function (req, file, cb){
+        cb(null, '/data/productsImages')
+    },
+    filename: function (req, file, cb){
+        cb(null, (imagesCounter + '_' + req.body.productName + '.jpg'))
+    }
+})).single('image'), 
+productController.edit); /* *****Edición de producto***** */
 
 module.exports = router; // Exportación ruteo
