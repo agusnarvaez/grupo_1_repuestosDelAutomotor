5603d2ff707d107b71109939211095e43236872e
juanpcarta <juanpcarta@gmail.com>
2022-01-11 04:54:36 -0300

    Modificaciones en el código Sql

--- db/structure.sql
+++ db/structure.sql
@@ -28,7 +28,7 @@ CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `categories` (
 
 LOCK TABLES `categories` WRITE;
 /*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-INSERT INTO `categories` VALUES (1,'Categoría A'),(2,'Categoría B'),(3,'Categoría C');
+INSERT INTO `categories` VALUES (1,'Piezas de motor'),(2,'Filtros y aceite'),(3,'Electroventiladores'),(4,'Frenos'),(5,'Repuestos de exterior'),(6,'Encendido');
 /*!40000 ALTER TABLE `categories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -70,7 +70,6 @@ CREATE TABLE `invoicing_addresses` (
 
 LOCK TABLES `invoicing_addresses` WRITE;
 /*!40000 ALTER TABLE `invoicing_addresses` DISABLE KEYS */;
-INSERT INTO `invoicing_addresses` VALUES (1,'Nueve de Julio','Buenos Aires','Tucumán',1566,NULL,NULL,6500,2),(2,'Mar del Plata','Buenos Aires','Avenida Cristobal Colón',4351,'10','b',4351,3),(3,'Comodoro Rivadavia','Chubut','Clarin',542,NULL,NULL,9002,1);
 /*!40000 ALTER TABLE `invoicing_addresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,7 +90,7 @@ CREATE TABLE `products` (
   PRIMARY KEY (`id`),
   KEY `FK_38813cbc-978f-4ffa-a4c4-81593354abc2` (`subcategory_id`),
   CONSTRAINT `FK_38813cbc-978f-4ffa-a4c4-81593354abc2` FOREIGN KEY (`subcategory_id`) REFERENCES `subcategories` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -100,7 +99,7 @@ CREATE TABLE `products` (
 
 LOCK TABLES `products` WRITE;
 /*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO `products` VALUES (1,'Producto 1','Descripción del producto 1…',1,1075,'fotoproducto1.jpg'),(2,'Producto 2','Descripción del producto 2…',2,3600,'fotoproducto2.jpg'),(3,'Producto 3','Descripción del producto 3…',3,1000,'fotoproducto3.jpg'),(4,'Producto 4','Descripción del producto 4…',4,2000,'fotoproducto4.jpg'),(5,'Producto 5','Descripción del producto 5…',5,5000,'fotoproducto5.jpg'),(6,'Producto 6','Descripción del producto 6…',6,7000,'fotoproducto6.jpg');
+INSERT INTO `products` VALUES (1,'Kit de distribución KTB108','Kit de distribución completo KTB108',1,5300,'fotoproducto1.jpg'),(2,'Kit de distribución con bomba KTBWP1960','Kit de distribución con bomba completo KTBWP1960',2,9600,'fotoproducto2.jpg'),(3,'Tensor ATB2323','Tensor de distribución ATB2323',3,3400,'fotoproducto3.jpg'),(4,'Correa de distribución 095SP190H ','Correa de distribución 095SP190H',4,2000,'fotoproducto4.jpg'),(5,'Producto 5','Descripción del producto 5...',5,5000,'fotoproducto5.jpg'),(6,'Producto 6','Descripción del producto 6...',5,7000,'1640183243508.jpg'),(7,'Producto Prueba ','Producto prueba Nº91920',3,9191,'1640348712155.jpg'),(8,'Kit de distribución KTB309','Kit de distribución completo KTB309',1,6700,'fotoproducto7.jpg'),(9,'Kit de distribución con bomba KTBWP3211','Kit de distribución con bomba completo KTBWP3211',2,12600,'fotoproducto8.jpg'),(10,'Tensor ATB2314','Tensor de distribución ATB2314',3,2700,'fotoproducto9.jpg'),(11,'Correa de distribución 095SP170H','Correa de distribución 095SP170H',4,1800,'fotoproducto10.jpg');
 /*!40000 ALTER TABLE `products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,7 +151,6 @@ CREATE TABLE `sales` (
 
 LOCK TABLES `sales` WRITE;
 /*!40000 ALTER TABLE `sales` DISABLE KEYS */;
-INSERT INTO `sales` VALUES (1,'2020-12-03 18:45:05',18000,3),(2,'2017-03-19 15:18:08',430000,1),(3,'2015-04-21 07:55:55',35000,2);
 /*!40000 ALTER TABLE `sales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -183,7 +181,6 @@ CREATE TABLE `sales_detail` (
 
 LOCK TABLES `sales_detail` WRITE;
 /*!40000 ALTER TABLE `sales_detail` DISABLE KEYS */;
-INSERT INTO `sales_detail` VALUES (1,18000,1,2,5),(2,430000,2,1,400),(3,35000,3,3,35);
 /*!40000 ALTER TABLE `sales_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -216,7 +213,6 @@ CREATE TABLE `shipping_adresses` (
 
 LOCK TABLES `shipping_adresses` WRITE;
 /*!40000 ALTER TABLE `shipping_adresses` DISABLE KEYS */;
-INSERT INTO `shipping_adresses` VALUES (1,'Nueve de Julio','Buenos Aires','Tucumán',1566,NULL,NULL,6500,2),(2,'Mar del Plata','Buenos Aires','Avenida Cristobal Colón',4351,'10','b',4351,3),(3,'Comodoro Rivadavia','Chubut','Clarin',542,NULL,NULL,9002,1);
 /*!40000 ALTER TABLE `shipping_adresses` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -243,7 +239,7 @@ CREATE TABLE `subcategories` (
 
 LOCK TABLES `subcategories` WRITE;
 /*!40000 ALTER TABLE `subcategories` DISABLE KEYS */;
-INSERT INTO `subcategories` VALUES (1,'Subcategoría 1',1),(2,'Subcategoría 2',1),(3,'Subcategoría 3',2),(4,'Subcategoría 4',2),(5,'Subcategoría 5',3),(6,'Subcategoría 6',3);
+INSERT INTO `subcategories` VALUES (1,'Kit de distribucion',1),(2,'Kit de distribución con bomba',1),(3,'Tensor',1),(4,'Correa de distribucion',1),(5,'Correa Poly-V',1),(6,'Filtro de aire ',2),(7,'Filtro de aceite',2),(8,'Filtro de nafta',2),(9,'Electroventilador',3),(10,'Helice',3),(11,'Forzador',3),(12,'Polea Viscosa',3),(13,'Pastillas de freno',4),(14,'Escobillas',5),(15,'Bujías',6);
 /*!40000 ALTER TABLE `subcategories` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -266,7 +262,7 @@ CREATE TABLE `users` (
   PRIMARY KEY (`id`),
   KEY `FK_7fa81561-bb77-4172-a156-e0c2025eb060` (`role_id`),
   CONSTRAINT `FK_7fa81561-bb77-4172-a156-e0c2025eb060` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -275,7 +271,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Agustín','Narvaez','agus.narvaez@outlook.com','Agus','$2a$10$j2exIjfT7qLVY7Q.C9/ireGVxfxsL4qC7oz2UNwtKlgTKnr7GzG42','1635958088645.jpg',1),(2,'Juan Pablo','Carta','juanpcarta@gmail.com','Juan','$2a$10$s6nyMNt4ajvHr0RUmZTj1.EucfVXOSitkqLvQm9lmjNe3I9iBPhqO','1635958175306.jpg',1),(3,'Nicolás','Di Renzo','nicodirenzo@gmail.com','Nico','$2a$10$DJl57omVzoqYfngIatJROOW2rK2QvWQ/OvtJzY.rbpL6bjbv94Y62','1635958242689.jpg',1);
+INSERT INTO `users` VALUES (1,'Juan Pablo','Carta','juanpcarta@gmail.com','Juanpcarta','$2a$10$Z8BLSDDEDT22AQb2WPlPoeyyWFrfSNqZlPlqROnbyqJNMqLyMs/YK','1641879732566.jpg',1),(2,'Agustín','Narvaez','agus.narvaez@outlook.com','Agus','$2a$10$nxqE.w.TwnokQph5jeQ22uGCnrdDcZaEnRMRH7f8Of0mNQmEL/RMK','1641880662603.jpg',1),(3,'Nicolás','Di Renzo','nicodirenzo@gmail.com','Nico','$2a$10$CAUiRbGOqG2HfybiyIEp4eRy4Jsx9FHVtS.IPepQpIV8VwjtGAFzG','1641880814326.jpg',1),(4,'Ana','Lopez','alopez@gmail.com','Ana','$2a$10$2QEzsqdKUtWJ4TZLJfSo6.ZxvEedjUNkkfg4gBHrLoYTZsSKAK6kK','1641882574166.jpg',1),(5,'Jorge','Roca','jroca@hotmail.com','Jorge','$2a$10$yBkezsGE50or19Lc9oNEt.udJ9AiypSkLgXz7HNv7w5wlPaO28C7C','1641882648004.jpg',1),(6,'Patricia','Rodriguez','prodriguez@gmail.com','Patri','$2a$10$QYHXGh5J8kAVESIlR7ZaJucqmMzkCGFLKc5L.aEuiBhFZYXswM8lK','1641882724180.jpg',1),(7,'Rubén','Tagliaferro','rtagliaferro@hotmail.com','Rubén','$2a$10$wPPek64z3nbmzSYT1kuTH.HIylw3mMtvJJ073DZub40IDnW9kKSiS','1641882786607.jpg',1),(8,'Esteban','Trotta','etrotta@gmail.com','Esteban','$2a$10$5qcjobHuo3m7qKOHkyC6putuvbSQOso7UQFeYIJKMK7M8d06gJm46','1641882886665.jpg',1),(9,'Agustina','Fernandez','afernandez@hotmail.com','Agustina','$2a$10$Ci0Xrov/.nfKhR5SZTxLoeGLGcXBr1zGE0gDNBJgqi3cfg/tO1TAK','1641882940148.jpg',1),(10,'Florencia','Carrizo','fcarrizo@hotmail.com','Flor','$2a$10$i1OekIxWYwEuzCV2ZgGWveekETvhi7z8XE6duhJ/N7qK9wWihCe/e','1641882978432.jpg',1),(11,'Marcos','Bernardez','mbernardez@yahoo.com','Marquitos','$2a$10$u21s6GIiJqgcq3AypN0fOegr0NrYNrkCVKixhKDiy295nIudhhYbK','1641883021442.jpg',1),(12,'Carlos','Ramirez','cramirez@yahoo.com','Charly','$2a$10$AaQlAaGkag3YNO0afSqbIempTBdDTMm5jcUI328R7A5Zj8Ei4LIbG','1641883517011.jpg',1),(13,'Sergio','Achaval','sachaval@hotmail.com','Serge','$2a$10$fcbcccU3yJ8E9rBf6xQorOwNumCXI/eL.y1KHzsvBGZfBgTXzVcNK','1641883582294.jpg',1),(14,'Yanina','Serrano','yserrano@hotmail.com','Yani','$2a$10$Hv2fAum5EQ/nKcmDxTXSYeP9.C9rd7kpxIhfH7PdjXkJ6gr1zA3D.','1641883634195.jpg',1),(15,'Susana','Horia','shoria@yahoo.com','Susi','$2a$10$qReVlt0hkO/MKM1ykSVnbuioc67/DKKbC1eZTev8kVRoMmshenrj2','1641883670558.jpg',1),(16,'Armando Esteban','Quito','aquito@hotmail.com','Armando','$2a$10$k6tK.YtULqYYAopswVBZu.lXxD9ic6BINTDnNndNUPqL9pGV2JA9G','1641883768338.jpg',1),(17,'Aquiles','Baeza','abaeza@yahoo.com','Aquiles','$2a$10$oU4pNxHXWfSBDF42FkJ/s.xD.sVdngZj.kJrUHoAn4eAXZ7hmIwKm','1641883816013.jpg',1),(18,'Elena','Nito','enito@hotmail.com','Elena','$2a$10$l/7bkfSiFe23Uk7nDW8thOQV.Sjv4v.DQopoURj9lFaOYIr1XGHCu','1641883889193.jpg',1),(19,'Elba','Lazo','elazo@hotmail.com','Elba','$2a$10$/mDyGXl.vPgro0XZsg63Ku1OYWwz27YU8m7gBnzsasA6vZeh9jyd6','1641883941004.jpg',1),(20,'Armando','Casas','acasas@yahoo.com','Armi','$2a$10$.z3bgkAcgGGUNX.zK4rqMuhI0HW4hHIHGqS3VtqzZUnXLhl5fCwzm','1641884029136.jpg',1);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -292,4 +288,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-24 11:54:40
+-- Dump completed on 2022-01-11  4:02:56
